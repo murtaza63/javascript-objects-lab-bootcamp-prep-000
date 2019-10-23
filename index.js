f03f668a1caa02418d4b6cc1672825a673b7ee90
breezy-constructor-6081 <murtaza63@gmail.com>
2019-10-23 06:50:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,5 +8,6 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
 }
 function deleteFromObjectByKey(object,key){
 var newObj = Object.assign({},Object);
-
+delete newObj[key];
+return newObj;
 }
