05002ad222f564ff6a997240bf229471b703bab0
breezy-constructor-6081 <murtaza63@gmail.com>
2019-10-23 06:42:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 var recipes = {};
-function updateObjectWithKeyValue(object,key,value){
-  return Object.assign({}, object, {[key]:value});
+function updateObjectWithKeyAndValue(object, key, value) {
+  return Object.assign({}, object, {[key]: value});
 }
